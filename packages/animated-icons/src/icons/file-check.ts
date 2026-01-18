110a65f6f9d1f5dc913960b5a89dcb644d8fbba3
Ajit <ajitzero@gmail.com>
2026-01-19 02:47:56 +0530

    fix: file-check icons

--- packages/animated-icons/src/icons/file-check.ts
+++ packages/animated-icons/src/icons/file-check.ts
@@ -88,7 +88,7 @@ export class FileCheckIcon {
 	handleMouseEnter(forced = false) {
 		if (forced || (!this.animate() && !this.isAnimating())) {
 			this.isAnimating.set(true);
-			this.#timer = setTimeout(() => this.isAnimating.set(false), 1400);
+			this.#timer = setTimeout(() => this.isAnimating.set(false), 500);
 		}
 	}
 
