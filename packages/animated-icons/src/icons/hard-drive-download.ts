195576b25c911df93c042ebf5d46a8eb9ecc2339
Ajit <ajitzero@gmail.com>
2026-01-23 18:24:07 +0530

    fix: hard-drive icons

--- packages/animated-icons/src/icons/hard-drive-download.ts
+++ packages/animated-icons/src/icons/hard-drive-download.ts
@@ -39,12 +39,15 @@ import { ANIMATED_ICONS_CONFIG } from '../tokens/provider';
 		:host {
 			display: inline-block;
 		}
-		g.animate path {
-			transform: translateY(2px);
-			transition: transform 0.3s cubic-bezier(0.68, -0.6, 0.32, 1.6);
+		.hard-drive-download-icon {
+			overflow: visible;
 		}
 		g path {
 			transform: translateY(0);
+			transition: transform 0.3s cubic-bezier(0.68, -0.6, 0.32, 1.6);
+		}
+		.hard-drive-download-icon.animate g path {
+			transform: translateY(2px);
 			transition: transform 0.3s cubic-bezier(0.68, -0.6, 0.32, 1.6);
 		}
 	`,
--- packages/animated-icons/src/icons/hard-drive-upload.ts
+++ packages/animated-icons/src/icons/hard-drive-upload.ts
@@ -39,15 +39,15 @@ import { ANIMATED_ICONS_CONFIG } from '../tokens/provider';
 		:host {
 			display: inline-block;
 		}
-		.hard-drive-upload {
+		.hard-drive-upload-icon {
 			overflow: visible;
 		}
-		g.animate path {
-			transform: translateY(-2px);
+		.hard-drive-upload-icon g path {
+			transform: translateY(0);
 			transition: transform 0.3s cubic-bezier(0.68, -0.6, 0.32, 1.6);
 		}
-		g path {
-			transform: translateY(0);
+		.hard-drive-upload-icon.animate g path {
+			transform: translateY(-2px);
 			transition: transform 0.3s cubic-bezier(0.68, -0.6, 0.32, 1.6);
 		}
 	`,
