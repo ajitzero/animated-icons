1eb3545c8e1a9209af023cc67f1b7391e87f69c2
Ajit <ajitzero@gmail.com>
2026-01-24 09:58:00 +0530

    fix: file-chart icons

--- packages/animated-icons/src/icons/file-chart-column-increasing.ts
+++ packages/animated-icons/src/icons/file-chart-column-increasing.ts
@@ -105,7 +105,7 @@ export class FileChartColumnIncreasingIcon {
 	handleMouseEnter(forced = false) {
 		if (forced || (!this.animate() && !this.isAnimating())) {
 			this.isAnimating.set(true);
-			this.#timer = setTimeout(() => this.isAnimating.set(false), 1400);
+			this.#timer = setTimeout(() => this.isAnimating.set(false), 700);
 		}
 	}
 
