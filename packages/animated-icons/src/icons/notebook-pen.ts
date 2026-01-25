1ec96d7e737c9108465b7e723865819871c6f691
Ajit <ajitzero@gmail.com>
2026-01-25 15:41:19 +0530

    fix: remaining N icons

--- packages/animated-icons/src/icons/notebook-pen.ts
+++ packages/animated-icons/src/icons/notebook-pen.ts
@@ -51,7 +51,7 @@ import { ANIMATED_ICONS_CONFIG } from '../tokens/provider';
 			transition: transform 0.25s ease-in-out;
 		}
 
-		.pen.animate {
+		.notebook-pen-icon.animate .pen {
 			animation: penWiggle 0.5s ease-in-out 2;
 		}
 
@@ -92,7 +92,7 @@ export class NotebookPenIcon {
 	handleMouseEnter(forced = false) {
 		if (forced || (!this.animate() && !this.isAnimating())) {
 			this.isAnimating.set(true);
-			this.#timer = setTimeout(() => this.isAnimating.set(false), 1400);
+			this.#timer = setTimeout(() => this.isAnimating.set(false), 1000);
 		}
 	}
 
