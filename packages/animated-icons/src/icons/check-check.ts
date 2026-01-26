d43d5401cf7bcf4857a5f01b01d331dcbdd5781b
Ajit <ajitzero@gmail.com>
2026-01-26 10:01:08 +0530

    fix: check icons

--- packages/animated-icons/src/icons/check-check.ts
+++ packages/animated-icons/src/icons/check-check.ts
@@ -87,7 +87,7 @@ export class CheckCheckIcon {
 	handleMouseEnter(forced = false) {
 		if (forced || (!this.animate() && !this.isAnimating())) {
 			this.isAnimating.set(true);
-			this.#timer = setTimeout(() => this.isAnimating.set(false), 1400);
+			this.#timer = setTimeout(() => this.isAnimating.set(false), 500);
 		}
 	}
 
